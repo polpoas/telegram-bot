c377874f083c536193f6b563a5d7df9d5a925fb9
polpoas <160572369+polpoas@users.noreply.github.com>
2025-02-13 09:39:42 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,22 @@ CRYPTO_PAYMENT_LINKS = {
     "35":  "cryptomus.cloud/?35",
     "75": "cryptomus.cloud/?75",
     "300": "cryptomus.cloud/?300",
+    "90": "cryptomus.cloud/?90",
+    "70": "cryptomus.cloud/?70",
+    "750": "cryptomus.cloud/?750",
+    "40": "cryptomus.cloud/?40",
+    "55": "cryptomus.cloud/?55",
+    "110": "cryptomus.cloud/?110",
+    "80": "cryptomus.cloud/?80",
+    "130": "cryptomus.cloud/?130",
+     "800": "cryptomus.cloud/?800",
+     "120": "cryptomus.cloud/?120",
+     "140": "cryptomus.cloud/?140",
+     "900": "cryptomus.cloud/?900",
+     "45": "cryptomus.cloud/?45",
+      "850": "cryptomus.cloud/?850",
+    "100": "cryptomus.cloud/?100"
+    
    }
 # База данных
 DB_NAME = "shop.db"
